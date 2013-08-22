aa6c8b677aa85d588e04c1d3bfcb3abd834d0b63
Matt Coffin <mcoffin@mogulworx.com>
2013-08-21 18:19:19 -0600

    Add swscale to avfilter find module

--- FindAVFilter.cmake
+++ FindAVFilter.cmake
@@ -2,6 +2,12 @@ include(LibAVFindComponent)
 
 libav_find_component("Filter")
 
+find_library(SWSCALE_LIBRARY
+	NAMES swscale libswscale
+	HINTS ${AVFilter_LIBRARY_DIRS}
+)
+list(APPEND AVFilter_LIBRARIES ${SWSCALE_LIBRARY})
+
 # Handle arguments
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(AVFilter
